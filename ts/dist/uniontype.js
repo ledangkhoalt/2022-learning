154301133093c6aa333e084676e25461d88ccf76
KhoaLD <ledangkhoalt@gmail.com>
2022-01-04 21:57:56 +0700

    union type

---
+++ ts/dist/uniontype.js
@@ -0,0 +1,6 @@
+var username = "Tom";
+var pageName = "1";
+pageName = 2;
+var errorMessage;
+var user = null;
+var someProp;
---
+++ ts/src/uniontype.ts
@@ -0,0 +1,16 @@
+
+let username: string = "Tom"
+
+let pageName: string | number = "1"
+pageName = 2
+
+let errorMessage: string | null;
+
+interface UserInterface {
+    name: string;
+    surname: string;
+}
+
+let user: UserInterface | null = null;
+
+let someProp: string | number | null | undefined | string [] | object;
